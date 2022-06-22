420a5fe6840d3400feadab2ad8bf71d8ce006570
Fábio Oliveira <fabioaurelio1269@gmail.com>
2022-06-21 22:33:01 -0300

    Adjust NavBar layout for desktop

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,10 +1,15 @@
 <template>
   <nav class="nav-bar">
-    <p class="nav-bar__steps">
+    <div class="nav-bar__steps">
       <a>chevron_left</a>
-      <span class="nav-bar__steps--heavy">Etapa {{currentStep}}</span>
-      <span> de {{totalSteps}}</span>
-    </p>
+      <div class="nav-bar__steps--short">
+        <span class="nav-bar__steps--heavy">Etapa {{currentStep}} </span>
+        <span> de {{totalSteps}}</span>
+      </div>
+      <div class="nav-bar__steps--long">
+        <span>Alterar forma de pagamento</span>
+      </div>
+    </div>
     <div class="nav-bar__title">
       <div class="nav-bar__image-wrapper">
         <img src="../assets/images/card-icon.png">
